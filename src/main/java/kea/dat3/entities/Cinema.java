555ad1a6522ad8086be79e2b9e54840e8556edf6
Murat Memis <3605716+Buttersail@users.noreply.github.com>
2022-05-05 20:41:29 +0200

    add ManyToMany relation

--- src/main/java/kea/dat3/entities/Cinema.java
+++ src/main/java/kea/dat3/entities/Cinema.java
@@ -4,10 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -26,6 +23,14 @@ public class Cinema {
 
     @OneToMany(mappedBy = "cinemaId", fetch = FetchType.EAGER)
     private Set<Hall> halls = new HashSet<>();
+
+    @ManyToMany(cascade = {CascadeType.ALL})
+    @JoinTable(
+            name = "Cinema_Movie",
+            joinColumns = {@JoinColumn(name = "cinema_id")},
+            inverseJoinColumns = {@JoinColumn(name = "movie_id")}
+    )
+    Set<Movie> movies = new HashSet<>();
 
     public Cinema(int id, String name, String address, int phoneNumber) {
         this.id = id;
