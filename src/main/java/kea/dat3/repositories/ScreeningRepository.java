a1b3ed5ece5a640ca821857072e3529bb7401f86
BenAtic-KEA <mark024e@stud.kea.dk>
2022-05-06 10:07:51 +0200

    added Jpa repo for Screening.java, Movie.java, Staff.java

---
+++ src/main/java/kea/dat3/repositories/ScreeningRepository.java
@@ -0,0 +1,7 @@
+package kea.dat3.repositories;
+
+import kea.dat3.entities.Screening;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScreeningRepository extends JpaRepository<Screening, Long> {
+}
---
+++ src/main/java/kea/dat3/repositories/StaffRepository.java
@@ -0,0 +1,7 @@
+package kea.dat3.repositories;
+
+import kea.dat3.entities.Staff;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StaffRepository extends JpaRepository <Staff, String> {
+}
