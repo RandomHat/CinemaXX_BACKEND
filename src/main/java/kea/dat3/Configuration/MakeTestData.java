1359395686fbd5cc2598b31f1cfc4dd270847be7
Simon Roswall <simonroswall@gmail.com>
2022-05-09 09:44:35 +0200

    WIP create testdata, added fetcher to get movie data and client4xx exception

--- src/main/java/kea/dat3/Configuration/MakeTestData.java
+++ src/main/java/kea/dat3/Configuration/MakeTestData.java
@@ -1,7 +1,10 @@
 package kea.dat3.Configuration;
 
+import kea.dat3.entities.Customer;
 import kea.dat3.entities.Person;
 import kea.dat3.entities.Role;
+import kea.dat3.entities.Staff;
+import kea.dat3.repositories.CustomerRepository;
 import kea.dat3.repositories.PersonRepository;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -14,24 +17,34 @@ import org.springframework.stereotype.Controller;
 public class MakeTestData implements ApplicationRunner {
 
     PersonRepository personRepository;
+    CustomerRepository customerRepository;
+    //StaffRepository staffRepository;
 
-    public MakeTestData( PersonRepository personRepository) {
+    public MakeTestData(PersonRepository personRepository, CustomerRepository customerRepository/*, StaffRepository staffRepository*/) {
         this.personRepository = personRepository;
+        this.customerRepository = customerRepository;
+        /*this.staffRepository = staffRepository;*/
     }
 
     public void makeUsers() {
-        Person user = new Person("user@mail.dk","user","test12");
-        Person admin = new Person("admin@mail.dk","admin","test12");
-        Person user_admin = new Person("user-admin@mail.dk","user_admin","test12");
-        user.addRole(Role.USER);
-        admin.addRole(Role.ADMIN);
-        user_admin.addRole(Role.USER);
-        user_admin.addRole(Role.ADMIN);
-        personRepository.save(user);
-        personRepository.save(admin);
-        personRepository.save(user_admin);
+        Customer cust1 = new Customer("user", "McUser", "12345678", "user@mail.dk", "test12");
+        Customer cust2 = new Customer("userin", "McUser", "12345679", "userin@mail.dk", "test12");
+        //Staff staff = new Staff("user-admin@mail.dk","user_admin","test12");
+        customerRepository.save(cust1);
+        customerRepository.save(cust2);
+        //staffRepository.save(staff);
         System.out.println("CREATED " + personRepository.count() + " TEST PERSONS");
     }
+
+    public void makeMovies(){
+
+
+
+    }
+
+    public void makeCinemaSetup(){}
+
+    public void makeTestBookings(){}
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
---
+++ src/main/java/kea/dat3/utils/Fetcher.java
@@ -0,0 +1,51 @@
+package kea.dat3.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kea.dat3.error.Client4xxException;
+import org.springframework.http.HttpStatus;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Map;
+
+public class Fetcher {
+    private String url;
+    private String api_token;
+    private String json;
+    private ObjectMapper mapper = new ObjectMapper();
+
+    Fetcher(String url){
+        this.url = url;
+        this.api_token = System.getenv("API_TOKEN");
+    }
+
+    public void fetch() throws URISyntaxException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(url))
+                .GET()
+                .headers("Content-Type", "application/json;charset=UTF-8", "Authorization", "Bearer " + api_token)
+                .build();
+
+        client.sendAsync(request, BodyHandlers.ofString())
+                .thenApply((response) -> {
+                    if (response.statusCode() != 200){
+                    throw new Client4xxException(response.body(), HttpStatus.valueOf(response.statusCode()));
+                    }
+                    return response.body();
+                }).thenAccept((data) -> json = data)
+                .join();
+        }
+
+    public Map<String, Object> getFetchedMap() throws JsonProcessingException{
+        return mapper.readValue(json, Map.class);
+    }
+
+    public String getJson(){
+        return json;
+    }
+}
