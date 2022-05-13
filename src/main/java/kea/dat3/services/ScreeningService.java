871a99f73b275a1d82f478989f3d57491edacaf1
Mark Hansen <Mark024e@stud.kea.dk>
2022-05-13 09:59:38 +0200

    Refactor ScreeningResponse

--- src/main/java/kea/dat3/services/ScreeningService.java
+++ src/main/java/kea/dat3/services/ScreeningService.java
@@ -1,20 +1,29 @@
 package kea.dat3.services;
 
 import kea.dat3.dto.*;
-import kea.dat3.entities.Screening;
-import kea.dat3.repositories.ScreeningRepository;
+import kea.dat3.entities.*;
+import kea.dat3.repositories.*;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class ScreeningService {
 
     ScreeningRepository screeningRepository;
+    MovieRepository movieRepository;
+    HallRepository hallRepository;
+    CinemaRepository cinemaRepository;
+    StaffRepository staffRepository;
 
 
-    public ScreeningService(ScreeningRepository screeningRepository){
+    public ScreeningService(ScreeningRepository screeningRepository, MovieRepository movieRepository, HallRepository hallRepository, CinemaRepository cinemaRepository, StaffRepository staffRepository) {
         this.screeningRepository = screeningRepository;
+        this.movieRepository = movieRepository;
+        this.hallRepository = hallRepository;
+        this.cinemaRepository = cinemaRepository;
+        this.staffRepository = staffRepository;
     }
 
     public ScreeningResponse getScreening(Long id, boolean isAdmin){
@@ -22,7 +31,8 @@ public class ScreeningService {
     }
 
     public ScreeningResponse addScreening(ScreeningRequest body) {
-        Screening screeningNew = screeningRepository.save(new Screening(body));
+        Screening screeningToBeSaved = createScreeningBody(body);
+        Screening screeningNew = screeningRepository.save(screeningToBeSaved);
         return createResponse(screeningNew.getId(),true);}
 
     private ScreeningResponse createResponse(Long id, boolean isAdmin){
@@ -33,5 +43,16 @@ public class ScreeningService {
         MovieResponse movieResponse = new MovieResponse(screening.getMovie());
         StaffResponse staffResponse = new StaffResponse(screening.getCreatedBy());
         return new ScreeningResponse(screening,movieResponse,staffResponse,cinemaResponse,hallResponse,isAdmin);
+    }
+
+    private Screening createScreeningBody(ScreeningRequest body){
+        int duration = body.getDuration();
+        LocalDateTime showtime = body.getShowTime();
+        Movie movie = movieRepository.getById(body.getMovieId());
+        Cinema cinema = cinemaRepository.getById(body.getCinemaId());
+        Hall hall = hallRepository.getById(body.getHallId());
+        Staff staff = staffRepository.getById(body.getUsername());
+
+        return new Screening(duration,showtime,movie,cinema,hall,staff);
     }
 }
